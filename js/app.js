013c7cc1b89781f0d193e4583ac1c93f1e9a7b29
rajkumar <rajkumar.gootam@gmail.com>
2016-10-24 15:15:07 +0530

    Created icon in sidebar & Added material card & created second row and completed

--- js/app.js
+++ js/app.js
@@ -1,20 +1,32 @@
-var angularApp = angular.module('winning', ['ngRoute', 'ngResource', 'ngMaterial', 'ngAnimate']);
-console.log('hi');
+var angularApp = angular.module('winning', ['ngRoute', 'ui.router', 'ngResource', 'ngMaterial', 'ngAnimate']);
 
-angularApp.config(function ($routeProvider){
-  $routeProvider
-  .when ('/',
-  {
-    templateUrl: 'list/dashboard.html',
-    controller: 'AppCtrl',
-    controllerAs: 'vm'
-  })
-  })
+angularApp.config(function($mdThemingProvider,$stateProvider) {
+            $mdThemingProvider.theme('default')
+              .primaryPalette('teal')
+              .accentPalette('orange');
+
+            $stateProvider
+            .state('dashboard', {
+              url:'/dashboard',
+              templateUrl:'list/dashboard.html',
+              controller:'AppCtrl as vm'
+            });
+          });
 
 
-angularApp.controller("AppCtrl",['$resource','$filter',function($resource,$filter){
+
+angularApp.controller("AppCtrl",['$scope','$resource','$filter',function($scope,$resource,$filter){
   var vm=this;
   console.log('hi');
+  $scope.items = [1, 2, 3, 4, 5, 6, 7];
+      $scope.selectedItem;
+      $scope.getSelectedText = function() {
+        if ($scope.selectedItem !== undefined) {
+          return "You have selected: Item " + $scope.selectedItem;
+        } else {
+          return "Please select an item";
+        }
+      };
 
 
 }])
